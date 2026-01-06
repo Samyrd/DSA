5c384a5fe593ad5cfe59a92fce4ef5407c07cd3d
Samrendra Kumar <154054931+Samyrd@users.noreply.github.com>
2026-01-06 20:08:49 +0530

    Create A15.C

---
+++ A15.C
@@ -0,0 +1,65 @@
+//1.Armstrong number I
+//#include<stdio.h>
+//#include<math.h>
+//int main(){
+//	int num;
+//	printf("Enter a number:\n");
+//	scanf("%d",&num);
+//	int count=0;
+//	int x=num;
+//	int sum=0;
+//	//calculation of digits..
+//	while(x){
+//		x=x/10;
+//		count++;
+//	}
+//	//finding the sum
+//	x=num;
+////	for(x=num;x;x=x/10){
+//	while(x){
+//		int digit=x%10;
+//		sum=sum+pow(digit,count);
+//		x=x/10;
+//	}
+//	//checking armstrong 
+//	if(sum==num){
+//		printf("Armstrong num+ber");
+//	}
+//	else{
+//		printf("Not a armstron number");
+//	}
+//}
+
+
+
+//1.Armstrong number II: within 1 t0 1000;
+//#include<stdio.h>
+//#include<math.h>
+//int main(){
+//	int num;
+//	for(num=1;num<1000;num++){
+//	int x=num;
+//	int count=0;
+//	int sum=0;
+//	//calculation of digits..
+//	while(x){
+//		x=x/10;
+//		count++;
+//	}
+//	//finding the sum
+//	x=num;
+////	for(x=num;x;x=x/10){
+//	while(x){
+//		int digit=x%10;
+//		sum=sum+pow(digit,count);
+//		x=x/10;
+//	}
+//	//checking armstrong 
+//	if(sum==num){
+//		printf("%d\n",num);
+//	}
+//}
+//}
+
+
+
