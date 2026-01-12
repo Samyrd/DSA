868a5c3b7990360704ba1c5ad69c5008ddfad1ce
Samrendra Kumar <154054931+Samyrd@users.noreply.github.com>
2026-01-12 22:43:03 +0530

    Intro to C++

---
+++ 2C.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello MySirG";
+	return 0;
+}
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello"<<endl<<"MySirG";
+	return 0;
+	
+}
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b,c;
+	cout<<"Enter two numbers:"<<endl;
+	cin>>a>>b;
+	c=a+b;
+	cout<<"Sum of"<<a<<" and "<<b<< " is"<<c;
+	return 0;
+}
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int r,area;
+	cout<<"Enter radius of circle:";
+	cin>>r;
+	area=3.14*r*r;
+	cout<<"Area of the circle with radius "<<r <<" is "<<area;
+	return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int l,b,h,volume;
+	cout<<"Enter length, breadth and height of Couboid";
+	cin>>l>>b>>h;
+	volume=l*b*h;
+	cout<<"Volume of the Cuboid is: "<<volume;
+	return 0;
+}
+
+
+
+
+
+
