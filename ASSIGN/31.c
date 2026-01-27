8c561cc8a801f047651e2afbcb0b51f37002e3d8
Samrendra Kumar <154054931+Samyrd@users.noreply.github.com>
2026-01-27 23:53:33 +0530

    Update 31.c

--- ASSIGN/31.c
+++ ASSIGN/31.c
@@ -50,3 +50,37 @@ int main() {
     return 0;
 }
 
+
+#include <stdio.h>
+void sortArray(int arr[], int n) {
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[5];
+    int i;
+
+    printf("Enter elements in the array:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    sortArray(arr, 5);
+
+    printf("Array after sorting:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
