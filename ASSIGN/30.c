a0295b4c5da760ac52cd7c29c81d9d2983485708
Samrendra Kumar <154054931+Samyrd@users.noreply.github.com>
2026-02-01 16:27:26 +0530

    Update 30.c

--- ASSIGN/30.c
+++ ASSIGN/30.c
@@ -182,6 +182,25 @@ int main() {
 
 
 
+// Iterate JavaScript Program to print alternate elements
+// of the array
+
+function getAlternates(arr) {
+    let res = [];
+    
+    // Iterate over all alternate elements
+    for (let i = 0; i < arr.length; i += 2) {
+        res.push(arr[i]);
+    }
+    return res;
+}
+
+
+// Driver Code
+const arr = [10, 20, 30, 40, 50];
+const res = getAlternates(arr);
+console.log(res.join(" "));
+
 
 
 
