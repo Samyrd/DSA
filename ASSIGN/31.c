ba4db60b04afb9e39b1d026bfbd824f0a277e3fd
Samrendra Kumar <154054931+Samyrd@users.noreply.github.com>
2026-02-05 23:41:20 +0530

    DO

--- ASSIGN/31.c
+++ ASSIGN/31.c
@@ -194,3 +194,59 @@ int main() {
     }
     return 0;
 }
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// A function to find a sorted sub-sequence of size 3
+vector<int> find3Numbers(vector<int> &arr)
+{
+    int n = arr.size();
+
+    // Fill smaller[] such that smaller[i] stores the
+    // index of a smaller element on the left side
+    vector<int> smaller(n, -1);
+    int min = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] <= arr[min])
+            min = i;
+        else
+            smaller[i] = min;
+    }
+
+    // Fill greater[] such that greater[i] stores the
+    // index of a greater element on the right side
+    vector<int> greater(n, -1);
+    int max = n - 1;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (arr[i] >= arr[max])
+            max = i;
+        else
+            greater[i] = max;
+    }
+
+    // Find the triplet
+    for (int i = 0; i < n; i++)
+    {
+        if (smaller[i] != -1 && greater[i] != -1)
+            return {arr[smaller[i]], arr[i], arr[greater[i]]};
+    }
+
+    // If no such triplet is found, return an empty vector
+    return {};
+}
+
+// Driver code
+int main()
+{
+    vector<int> arr = {12, 11, 10, 5, 6, 2, 30};
+    vector<int> res = find3Numbers(arr);
+    for (int x : result)
+        cout << x << " ";
+    return 0;
+}
