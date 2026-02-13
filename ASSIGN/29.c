8af8fa38815b6b67ff7923e0c84c2436150f2c2a
Samrendra Kumar <154054931+Samyrd@users.noreply.github.com>
2026-02-13 21:57:57 +0530

    Update 29.c

--- ASSIGN/29.c
+++ ASSIGN/29.c
@@ -208,3 +208,31 @@ const a = [3, 5, 2, 5, 2];
 const b = [2, 3, 5, 5, 2];
 
 console.log(checkEqual(a, b) ? 'true' : 'false');
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+char nonRep(string &s) {
+    int n = s.length();
+    for (int i = 0; i < n; ++i) {
+        bool found = false;
+
+        for (int j = 0; j < n; ++j) {
+            if (i != j && s[i] == s[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) 
+            return s[i];
+    }
+
+    return '$';
+}
+
+int main() {
+    string s = "racecar";
+    cout << nonRep(s);
+    return 0;
+}
