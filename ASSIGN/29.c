9b2d082f9e941eb4204bc3ab53765b51f9e7e663
Samrendra Kumar <154054931+Samyrd@users.noreply.github.com>
2026-02-17 22:52:33 +0530

    Update 29.c

--- ASSIGN/29.c
+++ ASSIGN/29.c
@@ -370,3 +370,58 @@ int main()
     return 0;
 }
 //Driver Code Ends
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int swapCount(string &s){
+    int ans=0;
+    
+    //To store count of '['
+    int count=0;
+    int n=s.size();
+    
+    for(int i=0;i<n;i++){
+        if(s[i]=='['){
+            count++;
+        }
+        else{
+            count--;
+        }
+        
+        //When count becomes less than 0
+        if(count<0){
+            
+            //Start searching for '[' from (i+1)th index
+            int j=i+1;
+            while(j<n){
+                
+                //When jth index contains '['
+                if(s[j]=='['){
+                    break;
+                }
+                j++;
+            }
+            
+            //Increment answer
+            ans+=j-i;
+            
+            //Set Count to 1 again
+            count=1;
+            
+            //Bring character at jth position to ith position
+            //and shift all character from i to j-1 
+            //towards right 
+            char ch=s[j];
+            for(int k=j;k>i;k--){
+                s[k]=s[k-1];
+            }
+            s[i]=ch;
+        }
+    }
+    return ans;
+}
+
