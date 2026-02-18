ce2f0a3e861baf9597efbb0141fba40aa15bc955
Samrendra Kumar <154054931+Samyrd@users.noreply.github.com>
2026-02-18 23:31:41 +0530

    Update 30.c

--- ASSIGN/30.c
+++ ASSIGN/30.c
@@ -220,6 +220,52 @@ int main()
     return 0;
 }
 
+// C++ program to count total anagram
+// substring of a string
+#include <bits/stdc++.h>
+using namespace std;
+
+// Total number of lowercase characters
+#define MAX_CHAR 26
+
+// Utility method to return integer index
+// of character 'c'
+int toNum(char c)
+{
+    return (c - 'a');
+}
+
+// Returns count of total number of anagram
+// substrings of string str
+int countOfAnagramSubstring(string str)
+{
+    int N = str.length();
+
+    // To store counts of substrings with given
+    // set of frequencies.
+    map<vector<int>, int> mp;
+
+    // loop for starting index of substring
+    for (int i=0; i<N; i++)
+    {
+        vector<int> freq(MAX_CHAR, 0);
+
+        // loop for length of substring
+        for (int j=i; j<N; j++)
+        {
+            // update freq array of current
+            // substring
+            freq[toNum(str[j])]++;
+
+            // increase count corresponding
+            // to this freq array
+            mp[freq]++;
+        }
+    }
+
+    // loop over all different freq array and
+    // aggregate substring count
+    int result 
 
 #include <stdio.h>
 
